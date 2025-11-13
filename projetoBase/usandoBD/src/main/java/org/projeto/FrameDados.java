e2143f58c96623f889f5568e84a01db3516595f2
theooyuji <theo.yama123@gmail.com>
2025-11-13 13:53:52 -0300

    muimui

--- projetoBase/usandoBD/src/main/java/org/projeto/FrameDados.java
+++ projetoBase/usandoBD/src/main/java/org/projeto/FrameDados.java
@@ -8,7 +8,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class FrameDados extends JFrame {
-    private final JPanel cnt;
+    //private final JPanel cnt;
+    private final JTabbedPane abas;
     private final MongoDatabase mongo;
     public FrameDados(MongoDatabase mongo){
         this.mongo = mongo;
@@ -18,50 +19,16 @@ public class FrameDados extends JFrame {
 
         PainelProduto p = new PainelProduto(this.mongo);
         PainelCategoria p1 = new PainelCategoria(this.mongo);
-        cnt = new JPanel(new CardLayout());
 
-        cnt.add(p,"produto");
-        cnt.add(p1,"categoria");
-
-        JMenuBar menu = new JMenuBar();
-        JMenu jMenu = new JMenu("Coleções");
-
-        JMenuItem item1 = new JMenuItem("Produtos");
-        item1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                mudaTela("produto");
-            }
-        });
-        JMenuItem item2 = new JMenuItem("Categorias");
-        item2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                mudaTela("categoria");
-            }
-        });
-        jMenu.add(item1);
-        jMenu.add(item2);
-        menu.add(jMenu);
+        abas = new JTabbedPane();
+        abas.add("Produtos",p);
+        abas.add("Categorias",p1);
 
         setLayout(new BorderLayout());
 
-        add(cnt,BorderLayout.CENTER);
+        add(abas,BorderLayout.CENTER);
 
         p.setVisible(false);
         p1.setVisible(false);
-
-        setJMenuBar(menu);
-    }
-    private void mudaTela(String atual){
-        CardLayout cl = (CardLayout) cnt.getLayout();
-        cl.show(cnt, atual);
-        if(atual.equals("produto")){
-            for(Component c : cnt.getComponents()){
-                if(c instanceof PainelProduto){
-                    ((PainelProduto) c).atualizaTela();
-                }
-            }
-        }
     }
 }
